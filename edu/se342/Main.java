115834067c7859f351826c8e454e36de2da93f8c
Curtis Cali <botkilla2k12@gmail.com>
2015-10-23 16:18:17 -0400

    Create workflow for TeamLead

--- edu/se342/Main.java
+++ edu/se342/Main.java
@@ -2,6 +2,7 @@ package edu.se342;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * Created by austin on 10/18/15.
@@ -9,25 +10,24 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        List<TeamLead> leads = new ArrayList<>(); // List of Team Leads
-        Manager pm = new Manager(null); //Create Manager
+        CountDownLatch startSignal = new CountDownLatch(13);
+        MeetingRoom conferenceRoom = new MeetingRoom();
+
+        Manager pm = new Manager(conferenceRoom, startSignal); //Create Manager
 
         // Create Team Leaders
-        for (int i = 1; i <= 4; i++) {
-            TeamLead leader = new TeamLead("TeamLead " + i + 1, null); // TeamLead (Team, EmployeeNumber)
-            leader.setManager(pm);
-            leads.add(leader);
 
+        for (int i = 1; i <= 3; i++) {
+            TeamLead leader = new TeamLead(String.format("TeamLead %d%d", i, 1), conferenceRoom, startSignal); // TeamLead (Team, EmployeeNumber)
+            leader.setManager(pm);
             pm.addTeamLead(leader);
 
             // Create developers for each lead
             for (int j = 2; j <= 4; j++) { // Developer number 2 - 4
-                Developer dev = new Developer("Developer " + i + j); // Developer (TeamNumber, EmployeeNumber)
+                Developer dev = new Developer(String.format("Developer %d%d", (i + 1), (j + 2))); // Developer (TeamNumber, EmployeeNumber)
                 dev.setTeamLead(leader);
                 leader.addDeveloper(dev);
             }
         }
-
-
     }
 }
--- edu/se342/TeamLead.java
+++ edu/se342/TeamLead.java
@@ -1,30 +1,87 @@
 package edu.se342;
 
 import java.util.ArrayList;
+
+import java.util.List;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * Created by curtis on 10/16/15.
  */
 public class TeamLead extends Leader<Developer> {
-    private int teamNumber;
+    private CountDownLatch teamArrival;
+
     private Manager manager;
     private MeetingRoom conferenceRoom;
     private List<Developer> myDevs;
 
-    public TeamLead(String leadName, MeetingRoom conferenceRoom) {
-        super(leadName);
+    public TeamLead(String leadName, MeetingRoom conferenceRoom, CountDownLatch arriveAtWork) {
+        super(leadName, arriveAtWork);
         this.conferenceRoom = conferenceRoom;
+
         this.myDevs = new ArrayList<>();
+        teamArrival = new CountDownLatch(4);
+
     }
 
     public void run() {
+        super.run();
+
+        int sleepTime = (int) (Math.random() * (TimeHelp.HALF_HOUR.ms() + 1));
+
+        elapseTime(sleepTime);
+
+        System.out.printf("%s has arrived at work\n", getName());
+
         try {
             manager.arriveForMorningStandup();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
+        System.out.printf("%s is in Morning Standup\n", getName());
+        elapseTime(TimeHelp.FIFTEEN_MINUTES.ms());
+        System.out.printf("%s has left Morning Standup\n", getName());
+
+        teamArrival.countDown();
+
+        while (teamArrival.getCount() > 0) {
+            elapseTime(TimeHelp.MINUTE.ms());
+        }
+
+        conferenceRoom.reserve(4);
+        notifySubordinatesOfMeeting();
+        elapseTime(TimeHelp.FIFTEEN_MINUTES.ms());
+        conferenceRoom.leave();
+
+        performRegularTasks((int)(TimeHelp.HOUR.ms() * 4.5));
+
+        int breakTime = (int)((Math.random() * TimeHelp.HALF_HOUR.ms()) + TimeHelp.HALF_HOUR.ms());
+        System.out.printf("%s is taking a lunch break\n", getName());
+
+        elapseTime(breakTime);
+
+        System.out.printf("%s has returned from lunch break\n", getName());
+
+        performRegularTasks((int)(TimeHelp.HOUR.ms() * 8));
+
+        try {
+            conferenceRoom.arriveInRoom();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        elapseTime(TimeHelp.FIFTEEN_MINUTES.ms());
+
+        int remainingWorkTime = (int)((Math.random() * 45) + 1);
+
+        performRegularTasks(getElapsedTime() + remainingWorkTime);
+        System.out.printf("%s has left work for the day\n", getName());
     }
 
     public void setManager(Manager manager) {
@@ -57,5 +114,30 @@ public class TeamLead extends Leader<Developer> {
             e.printStackTrace();
         }
 
+    }
+
+    private void performRegularTasks(int timeInMilliseconds) {
+        while(getElapsedTime() < timeInMilliseconds) {
+            Developer questionAsker = answerQuestion();
+
+            if (questionAsker != null) {
+                int probability = (int)((Math.random() * 1) + 1);
+
+                if (probability == 1) {
+                    System.out.printf(
+                        "%s has answered a question from %s\n",
+                        getName(),
+                        questionAsker.getName()
+                    );
+                } else {
+                    System.out.printf("%s cannot answer the question, %s asks the Project Manager");
+                    manager.requestAnswerForQuestion(this);
+                }
+            } else {
+                System.out.printf("%s is coding and testing\n", getName());
+            }
+
+            elapseTime(TimeHelp.MINUTE.ms());
+        }
     }
 }
